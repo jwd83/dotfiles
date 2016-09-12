0e120c81e3e0ad66ffd0aefd8cdc20bc3e22c42c
Jared <jared@quantumcodeworks.com>
2016-09-12 00:48:38 -0400

    Create setup-debian.sh

---
+++ setup-debian.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+apt update -y && apt upgrade -y
+
+apt install git htop iftop ufw mosh weechat strace dstat mc swapspace nodejs -y
+ufw allow 22
+ufw allow 60000:60100/udp
+ufw --force enable
+
+echo ""
+echo "quick setup included..."
+echo "------------------------------------------"
+echo "git htop iftop ufw mosh weechat strace dstat mc"
+echo "swapsace nodejs make"
+echo ""
+echo "firewall rules block all except"
+echo "------------------------------------------"
+echo "22          *   ssh"
+echo "60000-60100 udp mosh"
+echo ""
