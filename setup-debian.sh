ceaca578406fe6a9ba0f0c0adf290d2dabf3f16e
Jared <jared@quantumcodeworks.com>
2016-09-13 04:11:31 -0400

    Update setup-debian.sh

--- setup-debian.sh
+++ setup-debian.sh
@@ -8,15 +8,32 @@
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 apt update -y && apt upgrade -y
 
-apt install tmux git htop iftop ufw mosh weechat strace dstat mc swapspace nodejs make cmatrix build-essential proxychains tor -y
+# install packages
+
+apt install tmux git htop iftop ufw mosh weechat strace dstat mc swapspace nodejs make cmatrix build-essential proxychains -y
+
+# setup firewall rules
+
 ufw allow 22
 ufw allow 60000:60100/udp
 ufw --force enable
 
+# install rmate support
+
 curl -o /usr/local/bin/rmate https://raw.githubusercontent.com/aurora/rmate/master/rmate
 sudo chmod +x /usr/local/bin/rmate
 
+# install the slap editor
+
 npm install -g slap
+
+# install config files
+
+curl -o ~/.gitconfig https://raw.githubusercontent.com/jared0x90/dotfiles/master/.gitconfig
+curl -o ~/.iftoprc https://raw.githubusercontent.com/jared0x90/dotfiles/master/.iftoprc
+curl -o ~/.nanorc https://raw.githubusercontent.com/jared0x90/dotfiles/master/.nanorc
+curl -o ~/.tmux.conf https://raw.githubusercontent.com/jared0x90/dotfiles/master/.tmux.conf
+curl -o ~/.vimrc https://raw.githubusercontent.com/jared0x90/dotfiles/master/.vimrc
 
 echo ""
 echo "quick setup included..."
