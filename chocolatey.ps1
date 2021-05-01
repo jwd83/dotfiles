939e9799118d73e4de8a9bcdf834becdc3d9df7c
Jared <jared0x90@gmail.com>
2021-05-01 03:51:19 -0400

    Update chocolatey.ps1

--- chocolatey.ps1
+++ chocolatey.ps1
@@ -1,3 +1,6 @@
 # you must first install chocolately AND then run this as admin on a powershell after executing....
 # Set-ExecutionPolicy Bypass -Scope Process
 choco install -y 7zip autohotkey.portable autoruns bitwarden bitwarden-cli curl cutepdf firefox foxitreader git hwmonitor inkscape k-litecodecpackfull libreoffice-fresh nodejs notepadplusplus openssh paint.net putty python3 sumatrapdf.install vlc vscode wget winscp.install
+
+
+# Packages to look into winlogbeat
