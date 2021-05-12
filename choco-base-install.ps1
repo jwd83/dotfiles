2f7022dcaaed311e0d62cb895be4834d05807544
Jared <jared0x90@gmail.com>
2021-05-12 15:55:47 -0700

    Update and rename chocolatey.ps1 to choco-base-install.ps1

---
+++ choco-base-install.ps1
@@ -0,0 +1,4 @@
+# you must first install chocolately AND then run this as admin on a powershell after executing....
+# Set-ExecutionPolicy Bypass -Scope Process
+
+choco install -y 7zip autohotkey.portable autoruns bitwarden bitwarden-cli bleachbit ChocolateyGUI curl cutepdf discord firefox foxitreader git hwmonitor inkscape k-litecodecpackfull libreoffice-fresh nodejs notepadplusplus openssh paint.net putty python3 slack sublimetext3 sumatrapdf.install vlc vscode wget winscp.install zerotier-one
