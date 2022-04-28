1867d6bdc35698bd88076439cd2eb1ae0aeb40eb
Jared De Blander <jared@ayrscott.com>
2022-04-28 13:34:13 -0400

    Update setup-windows.bat

--- setup-windows.bat
+++ setup-windows.bat
@@ -1,1 +1,8 @@
-
+winget install Audacity.Audacity
+winget install TimKosse.FileZilla.Client
+winget install Git.Git
+winget install Microsoft.PowerToys
+winget install Microsoft.VisualStudioCode
+winget install Notepad++.Notepad++
+winget install VideoLAN.VLC
+winget install voidtools.Everything.Lite
